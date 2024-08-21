f8d6616e5014f84a45fed3ebdf3decba256d51f0
Zymbsis <aleksandra.aleksashka1809@gmail.com>
2024-08-21 23:36:43 +0200

    init repo

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const PORT = 3000;
+const app = express();
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Hello world!',
+  });
+});
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
